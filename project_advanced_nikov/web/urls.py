845ddf699e9b6f816b83ad4e5d8914788d8b6b21
Nikolay_Nikov <nikolainikov250@gmail.com>
2024-04-04 21:54:17 +0300

    #nachalo

--- project_advanced_nikov/web/urls.py
+++ project_advanced_nikov/web/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from project_advanced_nikov.web.views import index
+from project_advanced_nikov.web.views import index, forus, services, contact
 
 urlpatterns = (
     path("", index, name="index"),
-    # path("nachalo/", nachalo, name="nachalo"),
+    path("forus/", forus, name="forus"),
+    path("services/", services, name="services"),
+    path("contact/", contact, name="contact"),
 )
--- project_advanced_nikov/web/views.py
+++ project_advanced_nikov/web/views.py
@@ -8,6 +8,27 @@ def index(request):
     }
     return render(request, "web/index.html", context)
 
+def forus(request):
+    # profile = Profile.objects.first()
+    context = {
+       # 'profile':profile
+    }
+    return render(request, "web/forus.html", context)
+
+def contact(request):
+    # profile = Profile.objects.first()
+    context = {
+       # 'profile':profile
+    }
+    return render(request, "web/contact.html", context)
+
+def services(request):
+    # profile = Profile.objects.first()
+    context = {
+       # 'profile':profile
+    }
+    return render(request, "web/services.html", context)
+
 # def nachalo(request):
 #     context = {
 #
